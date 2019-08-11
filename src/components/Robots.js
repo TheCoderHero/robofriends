5a15f46902af05085ab9e253fca2f36e444c1ee0
John Tanner <thecoderhero@gmail.com>
2019-08-11 11:01:03 -0700

    Added robofriends source code

---
+++ src/components/Robots.js
@@ -0,0 +1,22 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'TheCoderHero',
+        email: 'thecoderhero@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Stylez',
+        email: 'stylez@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Webz',
+        email: 'webz@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Cpt_AJAX',
+        email: 'cptajax@gmail.com'
+    }
+]
